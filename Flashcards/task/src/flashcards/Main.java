fcd49ca13975fbf04bd11ac08074d62c4ca71090
lyapunovaa <lyapunovrabota@gmail.com>
2020-05-08 22:59:34 +0300

    Add methods game and createArrayOfCards

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -3,26 +3,55 @@ package flashcards;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
+    static String inputString() {
+        String input = scanner.nextLine();
+        return input;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        String term = scanner.nextLine();
-        String def = scanner.nextLine();
-        FlashCard flashCard = new FlashCard(term, def);//создаю конструктором
-        String ans = scanner.next();
 
-        game(flashCard, ans);
+        // FlashCard flashCard = new FlashCard(term, def);//создаю конструктором
+        //   String ans = scanner.next();
+
+        game(createArrayOfCards());
 
     }
 
 
-    public static void game(FlashCard flashCard, String ans) {
-        if (flashCard.checkAnswer(ans)) {             //Заглушка для прохождения задания
-            System.out.println("Your answer is right!");
-        } else {
-            System.out.println("Your answer is wrong...");
+    public static void game(FlashCard[] arrayOfCards) {
+        for (int i = 0; i <= arrayOfCards.length - 1; i++) {
+            System.out.println("Print the definition of \"" + arrayOfCards[i].getTerm() + "\"");
+            String ans = scanner.nextLine();
+            if (ans.equals(arrayOfCards[i].getDefinition())) {
+                System.out.println("Correct answer.");
+                continue;
+            } else {
+                System.out.println("Wrong answer. The correct one is \"" + arrayOfCards[i].getDefinition() + "\"");
+                continue;
+            }
         }
+
     }
+
+    public static FlashCard[] createArrayOfCards() {
+        System.out.println("Input the number of cards:");
+        int howMuchCardCreate = scanner.nextInt();
+        inputString();
+        FlashCard[] arrayOfCards = new FlashCard[howMuchCardCreate];
+        for (int numberOfCard = 1; numberOfCard <= howMuchCardCreate; numberOfCard++) {
+            System.out.println("The card #" + numberOfCard + ":");
+            String term = scanner.nextLine();
+            System.out.println("Print the definition of the card #" + numberOfCard + ":");
+            String def = scanner.nextLine();
+            FlashCard flashCard = new FlashCard(term, def);
+            arrayOfCards[numberOfCard - 1] = flashCard;
+        }
+        return arrayOfCards;
+    }
+
 }
 
 
